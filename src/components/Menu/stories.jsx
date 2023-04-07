23aa6fef66299b64539e8fa188ef513bba768290
Talyson Moreira <talyson.penha@gmail.com>
2023-04-07 18:43:27 -0300

    Menu component created

---
+++ src/components/Menu/stories.jsx
@@ -0,0 +1,36 @@
+import { Menu } from '.';
+
+import linksMock from '../NavLinks/mock';
+
+export default {
+  title: 'Menu',
+  component: Menu,
+  args: {
+    links: linksMock,
+    logoData: {
+      text: 'Logo',
+      link: '#target',
+      srcImg: '',
+    },
+  },
+  argTypes: {
+    links: {
+      table: {
+        disable: true,
+      },
+    },
+    logoData: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+};
+
+export const Template = (args) => {
+  return (
+    <div>
+      <Menu {...args} />
+    </div>
+  );
+};
