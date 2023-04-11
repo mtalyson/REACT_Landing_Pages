dcc38aa5d85bad702d6c8cb8c17279e4a846f78c
Talyson Moreira <talyson.penha@gmail.com>
2023-04-11 16:27:56 -0300

    GridContent component created

---
+++ src/components/GridContent/index.jsx
@@ -0,0 +1,27 @@
+import P from 'prop-types';
+import * as Styled from './styles';
+
+import { SectionBackground } from '../SectionBackground';
+import { TextComponent } from '../TextComponent';
+import { Heading } from '../Heading';
+
+export const GridContent = ({ title, html, background }) => {
+  return (
+    <SectionBackground background={background}>
+      <Styled.Container>
+        <Heading colorDark={!background} uppercase>
+          {title}
+        </Heading>
+        <Styled.Html>
+          <TextComponent>{html}</TextComponent>
+        </Styled.Html>
+      </Styled.Container>
+    </SectionBackground>
+  );
+};
+
+GridContent.propTypes = {
+  title: P.node.isRequired,
+  html: P.node.isRequired,
+  background: P.bool,
+};
--- src/styles/global-styles.js
+++ src/styles/global-styles.js
@@ -16,7 +16,22 @@ export const GlobalStyles = createGlobalStyle`
     font-family: ${({ theme }) => theme.font.family.default};
   }
 
-  h1 {
+  h1, h2, h3, h4, h5, h6 {
     font-family: ${({ theme }) => theme.font.family.title};
+    margin: ${({ theme }) => theme.spacings.large} 0;
+    font-weight: 900;
+  }
+
+  p {
+    margin: ${({ theme }) => theme.spacings.medium} 0;
+  }
+
+  ul, ol {
+    margin: ${({ theme }) => theme.spacings.medium};
+    padding: ${({ theme }) => theme.spacings.medium};
+  }
+
+  a {
+    color: ${({ theme }) => theme.colors.secondaryColor};
   }
 `;
