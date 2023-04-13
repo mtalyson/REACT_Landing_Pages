643e4645b94e9fc4b20f6fa804a38048d239d359
Talyson Moreira <talyson.penha@gmail.com>
2023-04-13 01:43:35 -0300

    code cleaning

--- src/components/GridTwoColumns/styles.js
+++ src/components/GridTwoColumns/styles.js
@@ -2,7 +2,7 @@ import styled, { css } from 'styled-components';
 import { Title } from '../Heading/styles';
 
 export const Container = styled.div`
-  ${({ theme, background }) => css`
+  ${({ theme }) => css`
     display: grid;
     grid-template-columns: 1fr 2fr;
     align-items: center;
@@ -27,9 +27,9 @@ export const TextContainer = styled.div`
 `;
 
 export const ImageContainer = styled.div`
-  ${({ theme }) => css``}
+  ${() => css``}
 `;
 
 export const Image = styled.img`
-  ${({ theme }) => css``}
+  ${() => css``}
 `;
